6f46dfb3d048ab430b18a48c5864179019dd32b9
Pratik Bhosale <pratikdbhosale2704@gmail.com>
2025-03-29 11:41:19 +0530

    deployed url changed

--- backend/server.js
+++ backend/server.js
@@ -48,6 +48,6 @@ app.get("/", (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
